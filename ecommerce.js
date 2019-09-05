4a02124c5a724b5e6e2cc999c0c67aea70e9dadf
IMMANUEL5015 <immanueldiai@gmail.com>
2019-09-05 22:22:01 +0100

    Submit codes for Ecommerce Project

---
+++ ecommerce.js
@@ -0,0 +1,21 @@
+//selecting the hamburger icon
+const menuToggler = document.getElementById("menu_toggler");
+let menuIsDisplayedOnPage = true; //nav menu is currently displayed on the page
+
+//decide what happens when the hamburger icon is clicked
+menuToggler.addEventListener("click", function(){
+    //toggle the navigation menu
+    if(menuIsDisplayedOnPage){
+        //remove the navigation menu if it is on the page
+       const navMenu = document.getElementById("menu");
+       navMenu.style.display = "none";
+       document.body.style.display = "block";
+       menuIsDisplayedOnPage = false; 
+    }else{
+        //add the nav menu back to the page.
+        const navMenu = document.getElementById("menu");
+        navMenu.style.display = "block";
+        document.body.style.display = "grid";
+        menuIsDisplayedOnPage = true; 
+    }
+});
