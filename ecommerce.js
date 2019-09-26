f8c7d779de7530f6893bfed0ef47ac7b6042cd81
IMMANUEL5015 <immanueldiai@gmail.com>
2019-09-27 00:42:54 +0100

    DRY up the code a little

--- ecommerce.js
+++ ecommerce.js
@@ -1,18 +1,17 @@
 //selecting the hamburger icon
 const menuToggler = document.getElementById("menu_toggler");
 let menuIsDisplayedOnPage = true; //nav menu is currently displayed on the page
+const navMenu = document.getElementById("menu");
 
 //decide what happens when the hamburger icon is clicked
 menuToggler.addEventListener("click", function(){
     //toggle the navigation menu
     if(menuIsDisplayedOnPage){
         //remove the navigation menu if it is on the page
-       const navMenu = document.getElementById("menu");
        navMenu.style.display = "none";
        document.body.style.display = "block";
     }else{
         //add the nav menu back to the page.
-        const navMenu = document.getElementById("menu");
         navMenu.style.display = "block";
         document.body.style.display = "grid"; 
     }
