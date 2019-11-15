c88c473716b25cc3e994c63f87a59ba81dba9c54
IMMANUEL5015 <immanueldiai@gmail.com>
2019-11-15 06:13:19 +0100

    Improve Toggling Effect

--- ecommerce.js
+++ ecommerce.js
@@ -1,10 +1,20 @@
 //selecting the hamburger icon
 const menuToggler = document.getElementById("menu_toggler");
 const navMenu = document.getElementById("menu");
+const body = document.getElementsByTagName("body")[0];
+
+var isVisible = false;
 
 //decide what happens when the hamburger icon is clicked
 menuToggler.addEventListener("click", function(){
     //toggle the navigation menu
-       navMenu.classList.toggle("navMenu");
-       document.body.classList.toggle("body");
+      if(isVisible){
+        navMenu.style.display = "none";  
+        body.style.display = "block";
+        menuToggler.style.display = "inline-block";
+      }else{
+          body.style.display = "grid";
+          navMenu.style.display = "inline-block";
+      }
+      isVisible = !isVisible; 
 });
